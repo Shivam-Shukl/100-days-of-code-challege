ee7eeb62c67057aab2d003ff74bbc1f788085395
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2023-11-08 21:54:19 +0530

    Add files via upload

---
+++ day8.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int sum = 0;
+        int maxSum = INT_MIN; 
+        for (int i = 0; i < n; ++i) 
+        {
+            sum += nums[i];
+            maxSum = max(maxSum, sum);
+
+            if (sum < 0)
+            sum = 0;
+        }
+
+        if (maxSum == 0)
+        {
+        
+            for (int i = 0; i < n; ++i) 
+            {
+                maxSum = max(maxSum, nums[i]);
+            }
+        }
+        return maxSum;
+
+
+    }
+
+    
+
+
+    
+};
