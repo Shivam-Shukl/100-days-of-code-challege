a341cbf32fd703d31c08f056969e0ae99116d6d9
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2023-12-24 09:46:10 +0530

    day53.cpp

--- day53.cpp
+++ day53.cpp
@@ -1,33 +1,102 @@
-// Beat  Time 85.71% and 100% Memory
-class Solution {
-public:
-    int amountOfTime(TreeNode* root, int start) {
-        vector<int> res(1,0);
-        dfs(root, start, res);
-        return res[0];
-    }
-    
-    int dfs(TreeNode* root, int start, vector<int>& res) {
-        if (root == nullptr) return 0;
-        int left = dfs(root->left, start, res);        
-        int right = dfs(root->right, start, res);
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 
-        if (root->val == start) {
-            int temp = max(abs(left), abs(right));
-            res[0] = max(res[0], temp);
-            return 0;
-        }
-        if (left > 0 || (root->left != nullptr && root->left->val == start)) {
-            int sum = left + abs(right) + 1;
-            res[0] = max(res[0], sum);
-            return left + 1;
-        } else if (right > 0 || (root->right != nullptr && root->right->val == start)) {
-            int sum = right + abs(left) + 1;
-            res[0] = max(res[0], sum);
-            return right + 1;
-        } 
+class Solution {
+  public:
+    TreeNode* createMapping(TreeNode* root, int target, map<TreeNode*,TreeNode*> &nodeToParent)
+    {
+        TreeNode* res = NULL;
+        queue<TreeNode*> q;
         
-        int sum = max(abs(left), abs(right));
-        return -sum - 1;
+        q.push(root);
+        nodeToParent[root]= NULL;
+        
+        while(!q.empty())
+        {
+            TreeNode* front =q.front();
+            q.pop();
+            
+            if(front -> val == target)
+            {
+                res = front;
+                
+            }
+            if(front->left)
+            {
+                nodeToParent[front -> left]=front;
+                q.push(front->left);
+            }
+            if(front->right)
+            {
+                nodeToParent[front -> right]=front;
+                q.push(front->right);
+            }
+        }
+        
+        return res;
+    }
+    int burnTree(TreeNode* root, map<TreeNode*,TreeNode*> &nodeToParent)
+    {
+        map<TreeNode* ,bool> visited;
+        queue<TreeNode*> q;
+        
+        q.push(root);
+        visited[root]=true;
+        
+        int ans= 0;
+        
+        while(!q.empty())
+        {
+            bool flag =0;
+            int size = q.size();
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* front =q.front();
+                q.pop();
+                if(front -> left && !visited[front->left])
+                {
+                    flag=1;
+                    q.push(front->left);
+                    visited[front->left]=1;
+                }
+                if(front -> right && !visited[front->right])
+                {
+                    flag=1;
+                    q.push(front->right);
+                    visited[front->right]=1;
+                }
+                if(nodeToParent[front] && !visited[nodeToParent[front]])
+                {
+                    flag=1;
+                    q.push(nodeToParent[front]);
+                    visited[nodeToParent[front]]=1;
+                }
+            }
+            if(flag==1)
+            {
+                ans++;
+            }
+            
+        }
+        return ans;
+    }
+    int amountOfTime(TreeNode* root, int start) 
+    {
+        // Your code goes here
+        map<TreeNode*,TreeNode*> nodeToParent;
+        TreeNode* targetNode = createMapping(root,start,nodeToParent);
+        
+        int ans = burnTree(targetNode,nodeToParent);
+        
+        return ans;
     }
 };
